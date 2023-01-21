96fe536f9dba3fcd7a8c69945f6546b5e17651cf
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-01-21 11:30:27 +0200

    nothing

---
+++ js/train.js
@@ -0,0 +1,39 @@
+const contacts = [
+  {
+    firstName: 'Akira',
+    lastName: 'Laine',
+    number: '0543236543',
+    likes: ['Pizza', 'Coding', 'Brownie Points'],
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    number: '0994372684',
+    likes: ['Hogwarts', 'Magic', 'Hagrid'],
+  },
+  {
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    number: '0487345643',
+    likes: ['Intriguing Cases', 'Violin'],
+  },
+  {
+    firstName: 'Kristian',
+    lastName: 'Vos',
+    number: 'unknown',
+    likes: ['JavaScript', 'Gaming', 'Foxes'],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.lengts; i++) {
+    if (name === contacts[i][firstName] && prop === contacts[i][prop]) {
+      return contacts[i][prop];
+    }
+  }
+  // Only change code above this line
+}
+
+lookUpProfile('Akira', 'likes');
+console.log()
