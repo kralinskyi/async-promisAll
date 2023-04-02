752b3b1bb10b9e25e60fc96e01ddc58071f805bb
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-02 13:24:13 +0300

    up

--- js/train.js
+++ js/train.js
@@ -1,39 +1,9 @@
-const contacts = [
-  {
-    firstName: 'Akira',
-    lastName: 'Laine',
-    number: '0543236543',
-    likes: ['Pizza', 'Coding', 'Brownie Points'],
-  },
-  {
-    firstName: 'Harry',
-    lastName: 'Potter',
-    number: '0994372684',
-    likes: ['Hogwarts', 'Magic', 'Hagrid'],
-  },
-  {
-    firstName: 'Sherlock',
-    lastName: 'Holmes',
-    number: '0487345643',
-    likes: ['Intriguing Cases', 'Violin'],
-  },
-  {
-    firstName: 'Kristian',
-    lastName: 'Vos',
-    number: 'unknown',
-    likes: ['JavaScript', 'Gaming', 'Foxes'],
-  },
-];
-
-function lookUpProfile(name, prop) {
-  // Only change code below this line
-  for (let i = 0; i < contacts.lengts; i++) {
-    if (name === contacts[i][firstName] && prop === contacts[i][prop]) {
-      return contacts[i][prop];
-    }
-  }
-  // Only change code above this line
+function addTogether() {
+  const [first, second] = arguments;
+  if (typeof first !== 'number') return undefined;
+  if (arguments.length === 1) return second => addTogether(first, second);
+  if (typeof second !== 'number') return undefined;
+  return first + second;
 }
 
-lookUpProfile('Akira', 'likes');
-console.log()
+addTogether(2)(5);
